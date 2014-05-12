08de4dcb576912b8ba7e0d4c679ac79ad88f62da
Dhruvesh Patel <pateld@phibro.com>
2014-05-12 11:11:57 -0400

    New comment is added

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace GitHubTest1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //This is test for code check-in
         }
     }
 }
